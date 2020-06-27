03df72fa47bb82107189ad47312e098c36a9d253
CrispyBoardgames <44710114+CrispyBoardgames@users.noreply.github.com>
2020-06-27 12:46:39 -0700

    NPC Movement "AI" Added

--- Assets/Scripts/NPCMovement.cs
+++ Assets/Scripts/NPCMovement.cs
@@ -1,10 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+
 // following Donovan Keith's code
 // http://www.donovankeith.com/2016/05/making-objects-float-up-down-in-unity/
+
+[RequireComponent(typeof(NavMeshAgent))]
 public class NPCMovement : MonoBehaviour
 {
+    
+
+    #region AnimationProperties
     //public float degreesPerSecond = 15.0f;
     // Rotation variables    
     public float speed = 2f;
@@ -15,15 +22,67 @@ public class NPCMovement : MonoBehaviour
     public float frequency = 1f;
     Vector3 posOffset = new Vector3();
     Vector3 tempPos = new Vector3();
+    #endregion
+
+
+    #region MovementProperties
+    NavMeshAgent Agent;
+    [SerializeField]
+    List<Vector3> PatrolPoints;
+    int PatrolIndex;
+    int PatrolIndexMax;
+
+    [SerializeField]
+    float WaitTime;
+    float DeltaTime;
+
+    #endregion
     // Start is called before the first frame update
     void Start()
     {
         // Store the starting position & rotation of theobject
         posOffset = transform.position;
+
+        Agent = this.GetComponent<NavMeshAgent>();
+
+        //Start on first linked point
+        PatrolIndex = 0;
+        PatrolIndexMax = PatrolPoints.Count;
     }
 
     // Update is called once per frame
     void Update()
+    {
+        IdleAnimation();
+        if(DeltaTime >= WaitTime)
+        {
+            Patrol();
+        }
+        //Updating time;
+        DeltaTime += Time.deltaTime;
+
+    }
+
+    #region Movement
+    //Patrol looks at the patrol points and picks the next one to walk to; rolls back to the first point in list.
+    //Can pause movement using isStopped property.
+    void Patrol()
+    {
+        if(PatrolIndexMax >= 1)
+        {
+            Agent.SetDestination(PatrolPoints[PatrolIndex]);
+            PatrolIndex = (PatrolIndex + 1) % PatrolIndexMax;
+        }
+        else
+        {
+            Debug.Log("Could not find point to move to.");
+        }
+    }
+
+    #endregion
+
+    #region AnimationMethods
+    void IdleAnimation()
     {
         // spin around Y-Axis
         //transform.Rotate(new Vector3(0f, Time.deltaTime * degreesPerSecond, 0f), Space.World);
@@ -33,4 +92,5 @@ public class NPCMovement : MonoBehaviour
         tempPos.y += Mathf.Sin(Time.fixedTime * Mathf.PI * frequency) * amplitude;
         transform.position = tempPos;
     }
+    #endregion
 }
