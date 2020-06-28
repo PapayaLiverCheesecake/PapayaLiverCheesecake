d22fd4cbf3c609a24f2382829524f8d8bc97b80d
Roselynn Conrady <conrady.roselynn@gmail.com>
2020-06-27 19:05:30 -0700

    toasting script (attach NPC)

---
+++ Assets/Scripts/ToastingScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToastingScript : MonoBehaviour
+{
+    bool isPlayerInRange;
+    public Transform player;
+    AudioSource m_AudioSource;
+    void Update()
+    {
+        if (isPlayerInRange && Input.GetMouseButtonDown(0))
+        {
+            StartCoroutine(theToasting());
+        }
+    }
+    void OnTriggerEnter (Collider other)
+    {
+        if (other.transform == player)
+        {
+            isPlayerInRange = true;
+        }
+    }
+    IEnumerator theToasting()
+    {
+        m_AudioSource.Play();
+        yield return new WaitForSeconds(0.5f);
+
+
+    }
+}
