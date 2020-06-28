5d57674e5d13d4fc35c17f66e93af24cc6dcd460
Tony Nguyen <tonynguyen1113@gmail.com>
2020-06-28 13:13:18 -0700

    Updated MenuScene
    Added functionality to the main menu.

---
+++ Assets/Scripts/EventManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class EventManager : MonoBehaviour
+{
+    public GameObject playButton;
+    public GameObject exitButton;
+    public GameObject mainMenuButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("FinalScene");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void ReturnToMainMenu()
+    {
+        SceneManager.LoadScene("BeginningMenuScene");
+    }
+}
